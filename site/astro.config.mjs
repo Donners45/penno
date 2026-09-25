36dd6e05aabd0f9bca2eebfe53ed4fca4e071d74
Donners45 <stdonnelly@btinternet.com>
2026-09-25 19:03:19 +0100

    github actions, and relative asset fix

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://donners45.github.io',
+  base: '/penno',
+});
